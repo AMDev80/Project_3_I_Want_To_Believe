15bc145273292c9903cf4ab6b61dc3ae94c76e28
AidanMDev1 <aidanmorgan@ufl.edu>
2023-12-02 21:43:53 -0500

    init

---
+++ Window.h
@@ -0,0 +1,7 @@
+//
+// Created by Anna Sicoli on 11/28/23.
+//
+#include <SFML/Graphics.hpp>
+#pragma once
+
+void startWindow();
---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "Window.h"
+#include "UFO.h"
+#include "MergeSort.h"
+#include "QuickSort.h"
+
+int main()
+{
+    // check to see if sorts work
+    UFOlist List("files/UFOPROJECTCSV.csv");
+    vector<UFOsighting> og_list = List.GetSightingsAt(40, -90).second; // test num
+    for (const auto& sight : og_list) {
+        cout << sight.date << " ";
+    }
+    cout << "\n";
+    quickSort(og_list, 0, og_list.size() - 1); // can be swapped with merge sort
+    for (const auto& sight : og_list) {
+        cout << sight.date << " ";
+    }
+
+
+    // Checks every grid tile
+//    int w = 0;
+//    for (int j = 23; j <= 54; j++) {
+//        for (int i = -128; i <= -65; i++) {
+//            // for each UFO sighting within grid tile
+//            for (const auto& sighting : List.GetSightingsAt(j, i).second) {
+//                // if valid sighting
+//                if (List.GetSightingsAt(j, i).first)
+//                    cout << ++w << '\n';
+//                // << " " << sighting.coordinate.first << " ," << sighting.coordinate.second << " , " << sighting.date << ", " << sighting.shape << " , " << sighting.duration << endl;
+//            }
+//        }
+//    }
+    startWindow();
+
+}
